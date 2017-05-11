ee92eca169fb7fa4f378a4cb7bfa8981154175a4
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-05-11 14:39:18 -0700

    file for organizing swc files by lab, region, species, cell type

---
+++ organize_swc.py
@@ -0,0 +1,23 @@
+import os
+from neuron_utils import neuron_info
+
+SWC_DIR = 'neuron_nmo'
+DATASET_DIR = 'datasets'
+
+def remove_spaces(string):
+    return string.replace(' ', '_')
+
+def organize_swc():
+    for filename in os.listdir(SWC_DIR):
+        if filename[-4:] == '.swc':
+            print filename
+            cell_type, species, region, lab = map(remove_spaces, neuron_info(filename))
+            mv_dir = '%s/%s/%s/%s/%s' % (DATASET_DIR, cell_type, species, region, lab)
+            os.system('mkdir -p %s' % mv_dir)
+            os.system('mv %s/%s %s/%s' % (SWC_DIR, filename, mv_dir, filename))
+
+def main():
+    organize_swc()
+
+if __name__ == '__main__':
+    main()
