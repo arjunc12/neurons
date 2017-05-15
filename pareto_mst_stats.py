ae9ad6612c09391d78b14077fccb70afb121ae36
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-05-15 11:37:41 -0700

    mask infinity values

--- pareto_mst_stats.py
+++ pareto_mst_stats.py
@@ -2,11 +2,15 @@ from collections import defaultdict
 import pandas as pd
 from pareto_mst_plots import COLUMNS
 import pylab
+from numpy.ma import masked_invalid
+
+def infmean(arr):
+    return pylab.mean(masked_invalid(arr))
 
 fname = 'pareto_mst.csv'
 df = pd.read_csv(fname, names=COLUMNS)
 total_trials = df['trials'].sum()
 total_successes = df['successes'].sum()
 print float(total_successes) / total_trials
-print pylab.mean(df['neural_dist'] / df['centroid_dist'])
-print pylab.mean(df['neural_dist'] / df['random_dist'])
+print infmean(df['neural_dist'] / df['centroid_dist'])
+print infmean(df['neural_dist'] / df['random_dist'])
