6ce29a0c00ed4bb4ab91deadb6d643264b8f9af1
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-05-19 13:29:27 -0700

    updated print statements

--- pareto_mst_stats.py
+++ pareto_mst_stats.py
@@ -11,6 +11,6 @@ fname = 'pareto_mst.csv'
 df = pd.read_csv(fname, names=COLUMNS)
 total_trials = df['trials'].sum()
 total_successes = df['successes'].sum()
-print float(total_successes) / total_trials
-print infmean(df['neural_dist'] / df['centroid_dist'])
-print infmean(df['neural_dist'] / df['random_dist'])
+print "p-value", float(total_successes) / total_trials
+print "neural to centroid ratio", infmean(df['neural_dist'] / df['centroid_dist'])
+print "neural to random ratio", infmean(df['neural_dist'] / df['random_dist'])
