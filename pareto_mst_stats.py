355d468f0c22b26606106970d30890513160f88b
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-06-07 14:07:22 -0700

    compare pareto tree to khuller tree

--- pareto_mst_stats.py
+++ pareto_mst_stats.py
@@ -14,3 +14,4 @@ total_successes = df['successes'].sum()
 print "p-value", float(total_successes) / total_trials
 print "neural to centroid ratio", infmean(df['neural_dist'] / df['centroid_dist'])
 print "neural to random ratio", infmean(df['neural_dist'] / df['random_dist'])
+print "dominate percentage", float(df['dominates'].sum()) / df['comparisons'].sum()
