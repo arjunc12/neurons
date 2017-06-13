785d6fad3133a87180218f759de9244baea68970
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-06-13 15:04:22 -0700

    skip file if api returns no data

--- organize_swc.py
+++ organize_swc.py
@@ -11,7 +11,12 @@ def organize_swc():
     for filename in os.listdir(SWC_DIR):
         if filename[-4:] == '.swc':
             print filename
-            cell_type, species, region, lab = map(remove_spaces, neuron_info(filename))
+            info = neuron_info(filename)
+            if info == None:
+                print "nothing"
+                continue
+            print info
+            cell_type, species, region, lab = map(remove_spaces, info)
             mv_dir = '%s/%s/%s/%s/%s' % (DATASET_DIR, cell_type, species, region, lab)
             os.system('mkdir -p %s' % mv_dir)
             os.system('mv %s/%s %s/%s' % (SWC_DIR, filename, mv_dir, filename))
