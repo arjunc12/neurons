d528bb80daf51e814a07fb7b44a3de5a24a77519
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2017-06-21 11:40:51 -0700

    separate module for kruskal's algorithm

---
+++ kruskal.py
@@ -0,0 +1,37 @@
+def makes_cycle(u, v, node_to_forest):
+    f1 = node_to_forest[u]
+    f2 = node_to_forest[v]
+    return f1 == f2
+ 
+def combine_forests(u, v, node_to_forest, forest_to_nodes, forest_to_edges):
+    f1 = node_to_forest[u]
+    f2 = node_to_forest[v]
+    if f1 == f2:
+        return
+    new_forest = min(f1, f2)
+    old_forest = max(f1, f2)
+    update_nodes = forest_to_nodes[old_forest]
+    update_edges = forest_to_edges[old_forest]
+    for node in update_nodes:
+        node_to_forest[node] = new_forest
+    forest_to_nodes[new_forest] += update_nodes
+    forest_to_edges[new_forest] += update_edges
+    forest_to_edges[new_forest].append((u, v))
+    del forest_to_nodes[old_forest]
+    del forest_to_edges[old_forest]
+    
+def kruskal(nodes, edges):
+    node_to_forest = {}
+    forest_to_nodes = {}
+    forest_to_edges = {}
+    
+    for i, u in enumerate(nodes):
+        node_to_forest[u] = i
+        forest_to_nodes[i] = [u]
+        forest_to_edges[i] = []
+        
+    for u, v in edges:
+        if not makes_cycle(u, v, node_to_forest):
+            combine_forests(u, v, node_to_forest, forest_to_nodes, forest_to_edges)
+            
+    return sum(forest_to_edges.values(), [])
