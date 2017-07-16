6809ca0c0c3862af8c5687957d7faaf6ba530d04
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-07-15 18:26:38 -0700

    changed input/output file names

--- neuron_builder.py
+++ neuron_builder.py
@@ -1,17 +1,21 @@
 import os
+from neuron_utils import get_neuron_points, viz_tree
 
 BUILDER_EXE = 'neuronbuilder2'
 
 OUTDIR = 'neuron_builder'
+OUTFILE = 'neuron_builder.swc'
 
-def build_neuron(radius=1, output='output.swc'):
-    build_command = '%s/%s %f > %s/%s' % (OUTDIR, BUILDER_EXE, radius,\
-                                          OUTDIR, output)
+def build_neuron(radius=1):
+    build_command = './%s %f > %s' % (BUILDER_EXE, radius, OUTFILE)
     print build_command
     os.system(build_command)
+    G = get_neuron_points(OUTFILE)[0]
+    return G
 
 def main():
-    build_neuron()
+    G = build_neuron(1)
+    viz_tree(G, 'neuron_builder', OUTDIR)
 
 if __name__ == '__main__':
     main()
