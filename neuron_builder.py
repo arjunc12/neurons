f0aacb0173f6e7036a3fa97d90f384e685dff84a
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-07-15 19:00:37 -0700

    fixed bug in which graph it returns; checks if graph is a tree

--- neuron_builder.py
+++ neuron_builder.py
@@ -1,5 +1,6 @@
 import os
-from neuron_utils import get_neuron_points, viz_tree
+from neuron_utils import get_neuron_points, viz_tree, is_tree
+from sys import argv
 
 BUILDER_EXE = 'neuronbuilder2'
 
@@ -10,11 +11,12 @@ def build_neuron(radius=1):
     build_command = './%s %f > %s' % (BUILDER_EXE, radius, OUTFILE)
     print build_command
     os.system(build_command)
-    G = get_neuron_points(OUTFILE)[0]
+    G = get_neuron_points(OUTFILE)[1]
+    assert is_tree(G)
     return G
 
 def main():
-    G = build_neuron(1)
+    G = build_neuron(float(argv[1]))
     viz_tree(G, 'neuron_builder', OUTDIR)
 
 if __name__ == '__main__':
