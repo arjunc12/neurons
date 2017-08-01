e20df3444e516677c013ef63b701e4175c5ebfba
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-08-01 15:50:39 -0700

    use genetic to generate family of solutions instead of particular solution

--- test_runtimes.py
+++ test_runtimes.py
@@ -4,7 +4,7 @@ mpl.use('agg')
 import pylab
 from pareto_functions import pareto_prim, pareto_genetic
 from time import time
-from cost_functions import graph_costs
+from cost_functions import graph_costs, normalize_cost
 from neuron_utils import complete_graph, pareto_cost, sort_neighbors, is_tree
 from random_graphs import random_point_graph
 import argparse
@@ -13,19 +13,19 @@ import pandas as pd
 MIN_POINTS = 8
 MAX_POINTS = 50
 
-COLNAMES = ['num_points', 'time1', 'time2', 'comparisons', 'dominates']
+COLNAMES = ['num_points', 'time1', 'time2'] #, 'comparisons', 'dominates']
 
 def runtimes_stats():
     df = pd.read_csv('test_runtimes.csv', names=COLNAMES)
-    print "greedy success rate", float(sum(df['dominates'])) / float(sum(df['comparisons']))
+    #print "greedy success rate", float(sum(df['dominates'])) / float(sum(df['comparisons']))
     df = df[['num_points', 'time1', 'time2']]
     df = df.groupby('num_points', as_index=False).agg(pylab.mean)
     pylab.figure()
-    time1 = df['time1'] * 60
-    time2 = df['time2'] * 60
+    time1 = df['time1']
+    time2 = df['time2']
     pylab.plot(df['num_points'], time1, c='b', label='greedy')
     pylab.scatter(df['num_points'], time2, c='r', label='genetic')
-    pylab.ylim(ymin=-1)
+    #pylab.ylim(ymin=-1)
     pylab.legend(loc=2)
     pylab.xlabel('number of points')
     pylab.ylabel('rumtime (minutes)')
@@ -41,11 +41,21 @@ def time_function(G, alpha, pareto_func):
     return mcost, scost, runtime
 
 def test_runtimes(num_iters=10, min_points=MIN_POINTS, max_points=MAX_POINTS):
+    
     for i in xrange(num_iters):
         print "iteration", i
         for num_points in xrange(min_points, max_points + 1):
             print "num_points", num_points
             G = random_point_graph(num_points=num_points)
+            
+            sat_tree = satellite_tree(G)
+            span_tree = nx.minimum_spanning_tree(G, weight='length')
+                   
+            opt_scost = float(satellite_cost(sat_tree))
+            normalize_scost = lambda cost : normalize_cost(cost, opt_scost)
+            opt_mcost = float(mst_cost(span_tree))
+            normalize_mcost = lambda cost : normalize_cost(cost, opt_mcost)
+            
             sort_neighbors(G)
             
             mcosts1 = []
@@ -63,22 +73,30 @@ def test_runtimes(num_iters=10, min_points=MIN_POINTS, max_points=MAX_POINTS):
             for alpha in pylab.arange(delta, 1, delta):
                 print "alpha", alpha
                 mcost1, scost1, time1 = time_function(G, alpha, pareto_prim)
-                mcost2, scost2, time2 = time_function(G, alpha, pareto_genetic)
 
-                mcosts1.append(mcost1)
-                mcosts2.append(mcost2)
-                
+                mcost1 = normalize_mcost(mcost1)
+                scost1 = normalize_scost(scost1)
+
+                mcosts1.append(mcost1)                
                 scosts1.append(scost1)
-                scosts2.append(scost2)
 
                 times1.append(time1)
-                times2.append(time2)
 
-                comparisons += 1
-                cost1 = pareto_cost(mcost1, scost1, alpha)
-                cost2 = pareto_cost(mcost2, scost2, alpha)
-                if cost1 <= cost2:
-                    dominates += 1
+            
+            greedy_runtime = sum(times1)
+
+            genetic_start = time()
+            genetic_trees = pareto_genetic(G)
+            genetic_end = time()
+
+            genetic_runtime = (genetic_end - genetic_start) / 60.0
+
+            for mcost2, scost2, T in genetic_trees:
+                mcost2 = normalize_mcost(mcost2)
+                scost2 = normalize_scost(scost2)
+                
+                mcosts2.append(mcost2)
+                scosts2.append(scost2)
 
             pylab.figure()
             pylab.scatter(mcosts1, scosts1, c='b', label='greedy')
@@ -95,10 +113,7 @@ def test_runtimes(num_iters=10, min_points=MIN_POINTS, max_points=MAX_POINTS):
             
             pylab.close()
 
-            time1 = pylab.mean(times1)
-            time2 = pylab.mean(times2)
-
-            write_items = [num_points, time1, time2, comparisons, dominates]
+            write_items = [num_points, greedy_runtime, genetic_runtime]
             
             write_items = map(str, write_items)
             write_items = ', '.join(write_items)
