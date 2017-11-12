e60f5bb0129e85d4b2fbbfa2488281a64171ee76
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-11-12 09:27:11 -0800

    changed minimum number of points

--- pareto_steiner_icc.R
+++ pareto_steiner_icc.R
@@ -1,24 +1,40 @@
 library(ICC)
 library(stringr)
 
+MIN_COUNT = 25
+MIN_POINTS = 100
+
 getICCs <- function(df)
 {
     for (cat in c("cell_type", "species", "region"))
     {
         df2 = unique(df, by=c("name", cat))
         df2$count = ave(df2$alpha, df2[,cat], FUN=length)
-        df2 = df2[df2$count > 25,]
+        df2 = df2[df2$count > MIN_COUNT,]
         print(cat)
         print(ICCbare(x=cat, y=alpha, data=df2))
     }
 }
 
-colnames = c('name', 'cell_type', 'species', 'region', 'lab', 'points', 'alpha',
-             'neural_dist', 'centroid_dist', 'random_dist', 'trials', 
-             'successes', 'comparisons', 'dominates')
-df = read.csv('pareto_mst.csv', col.names=colnames)
+
+#COLUMNS = ['name', 'cell_type', 'species', 'region', 'lab', 'points', 'alpha',\
+#           'norm_alpha', 'neural_dist', 'centroid_dist', 'random_dist',\
+#           'norm_neural_dist', 'norm_centroid_dist', 'norm_random_dist',\
+#           'trials', 'successes', 'norm_successes']
+
+#cnames = c('name', 'cell_type', 'species', 'region', 'lab', 'points', 'alpha',
+#             'neural_dist', 'centroid_dist', 'random_dist', 'trials', 
+#             'successes')
+
+cnames = c('name', 'cell_type', 'species', 'region', 'lab', 'points', 'alpha',
+           'norm_alpha', 'neural_dist', 'centroid_dist', 'random_dist',
+           'norm_neural_dist', 'norm_centroid_dist', 'norm_random_dist',
+           'trials', 'successes', 'norm_successes')
+df = read.csv('pareto_steiner.csv', col.names=cnames, header=FALSE)
+df = unique(df, by=c("name", "cell_type", "species", "region"))
 #print(df$cell_type == df$cell_type[3541])
 #df = df[df$cell_type != df$cell_type[3541],]
+df = df[df$points >= MIN_POINTS,]
 
 df$neuron_type = str_sub(df$name, -1, -1)
 
