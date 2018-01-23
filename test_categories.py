3d2039be1316c7c531e5766b8a9e283fe05911d9
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2018-01-23 12:05:59 -0800

    code for testing if two neurons have different mean alphas

---
+++ test_categories.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from scipy.stats import ttest_ind
+from pareto_steiner_stats import get_df
+import argparse
+from itertools import combinations
+
+def test_cat_vals(df, category, vals):
+    df2 = df.drop_duplicates(subset='name')
+    df2 = df2[df2[category].isin(vals)]
+    alphas = df2['alpha']
+    for val1, val2 in combinations(vals, 2):
+        sample1 = alphas[df2[category] == val1]
+        sample2 = alphas[df2[category] == val2]
+        degf = len(sample1) + len(sample2) - 2
+        print val1, val2, len(sample1), len(sample2), degf
+        print ttest_ind(sample1, sample2, equal_var=False)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--species', nargs='+', default=None)
+    parser.add_argument('-r', '--regions', nargs='+', default=None)
+    parser.add_argument('-c', '--cell_types', nargs='+', default=None)
+    parser.add_argument('-d', '--data_file', default='pareto_steiner.csv')
+
+    args = parser.parse_args()
+    species = args.species
+    regions = args.regions
+    cell_types = args.cell_types
+    data_file = args.data_file
+
+    df = get_df(data_file)
+
+    for category, vals in zip(['species', 'region', 'cell_type'], [species, regions, cell_types]):
+        if vals != None:
+            test_cat_vals(df, category, vals)
+
+if __name__ == '__main__':
+    main()
