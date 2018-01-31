ab7c4618991761bde95b76623649f1fdecdcc4a8
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2018-01-31 13:58:53 -0800

    merge files when plotting

--- parse_neurolucida.py
+++ parse_neurolucida.py
@@ -14,7 +14,7 @@ def split_swc(fname):
     curr_file = 0
     assert fname[-4:] == '.swc'
     prefix = fname[:-4]
-    for line in open(fname):
+    for line in open('asc_files/' + fname):
         if line[0] == '#':
             continue
         else:
@@ -23,7 +23,7 @@ def split_swc(fname):
             assert len(line) == 7
             if line[-1] == '-1':
                 if curr_lines != None:
-                    outname = prefix + str(curr_file) + '.swc'
+                    outname = 'swc_files/' + prefix + str(curr_file) + '.swc'
                     write_swc(curr_lines, outname)
                 curr_lines = [line]
                 curr_file += 1
@@ -31,18 +31,16 @@ def split_swc(fname):
                 curr_lines.append(line)
 
 def convert_asc():
-    for fname in os.listdir('.'):
+    for fname in os.listdir('asc_files'):
         prefix = fname[:-4]
         suffix = fname[-4:]
         print prefix, suffix
         if suffix == '.ASC':
-            os.system('./NLMorphologyConverter %s.ASC %s.swc SWC' % (prefix, prefix))
+            os.system('asc_files/NLMorphologyConverter asc_files/%s.ASC asc_files/%s.swc SWC' % (prefix, prefix))
             split_swc(prefix + '.swc')
 
 def main():
-    os.chdir('boutons/asc_files')
-    #os.system('./NLMorphologyConverter G419g#1-E.ASC G419g#1-E.swc SWC')
-    #split_swc('G419g#1-E.swc')
+    os.chdir('boutons')
     convert_asc()
 
 if __name__ == '__main__':
