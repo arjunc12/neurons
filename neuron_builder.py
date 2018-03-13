3b0389cadaf0f84634b241e31cf4d4a062373792
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2018-03-13 14:08:37 -0700

    separated my algorithm from snider's

--- neuron_builder.py
+++ neuron_builder.py
@@ -8,13 +8,16 @@ BUILDER_EXE = 'neuronbuilder2'
 OUTDIR = 'neuron_builder'
 OUTFILE = 'neuron_builder.swc'
 
-def build_neuron(radius=1):
+def build_neuron_snider(radius=1):
     build_command = './%s %f > %s' % (BUILDER_EXE, radius, OUTFILE)
     print build_command
     os.system(build_command)
     G = get_neuron_points(OUTFILE)[1]
     assert is_tree(G)
     return G
+
+def build_neuron():
+    pass
 
 def main():
     G = build_neuron(float(argv[1]))
