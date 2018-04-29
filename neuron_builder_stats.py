f7c81770f7ddfcc4f643328bc50824781fc5a74d
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2018-04-29 14:32:31 -0700

    changed where files are read from; added basic stats

--- neuron_builder_stats.py
+++ neuron_builder_stats.py
@@ -3,6 +3,18 @@ mpl.use('agg')
 import pylab
 import pandas as pd
 
+OUTPUT_DIR = '/iblsn/data/Arjun/neurons/neuron_builder/snider/output'
+
+PARAMETERS = {'snider': ['puncta radius', 'remove radius', 'trial length']}
+
+def get_dfs():
+    steiner_df = pd.read_csv('%s/pareto_steiner.csv' % OUTPUT_DIR, skipinitialspace=True)
+    models_df = pd.read_csv('%s/models.csv' % OUTPUT_DIR, skipinitialspace=True)
+    parameters_df = pd.read_csv('%s/parameters.csv' % OUTPUT_DIR, skipinitialspace=True)
+    parameters_df = parameters_df[['tree'] + PARAMETERS['snider']]
+
+    return steiner_df, models_df, parameters_df
+
 def radius_plot(df):
     pylab.figure()
     pylab.scatter(df['radius'], df['alpha'])
@@ -17,15 +29,14 @@ def alphas_distribution(df):
     pylab.savefig('steiner_stats/neuron_builder_alphas.pdf', format='pdf')
     pylab.close()
 
-def neuron_builder_stats(df):
-    print "mean alpha", pylab.mean(df['alpha'])
-    print "mean dist", pylab.mean(df['dist'])
+def neuron_builder_stats(steiner_df, models_df):
+    print pylab.mean(steiner_df['alpha'])
+    df = models_df[models_df['model'] == 'neural']
+    print pylab.mean(df['dist'])
 
 def main():
-    df = pd.read_csv('neuron_builder.csv', names=['radius', 'size', 'dist', 'alpha'])
-    radius_plot(df)
-    alphas_distribution(df)
-    neuron_builder_stats(df)
+    steiner_df, models_df, parameters_df = get_dfs()
+    neuron_builder_stats(steiner_df, models_df)
 
 if __name__ == '__main__':
     main()
