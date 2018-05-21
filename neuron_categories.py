b2f8fa8d00c2827f0617142356b35b25603b8e85
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2018-05-21 16:43:15 -0700

    write filtered categories

--- neuron_categories.py
+++ neuron_categories.py
@@ -1,7 +1,15 @@
 import os
+import pandas as pd
 
 CATEGORIES_FILE = '/iblsn/data/Arjun/neurons/neuron_categories/neuron_categories.csv'
+CATEGORIES_FILE_FILTERED = '/iblsn/data/Arjun/neurons/neuron_categories/neuron_categories_filtered.csv'
 DATASETS_DIR = '/iblsn/data/Arjun/neurons/datasets'
+
+def remove_sep(string, sep='_'):
+    return string.replace(sep, ' ')
+
+def add_count_col(df, categories):
+    return df.groupby(categories).size().reset_index(name='count')
 
 def remove_commas(string):
     return string.replace(',', '')
@@ -22,8 +30,32 @@ def write_categories():
                             write_items = ', '.join(write_items)
                             f.write('%s\n' % write_items)
 
+def filter_categories():
+    df = pd.read_csv(CATEGORIES_FILE, skipinitialspace=True)
+    categories = ['cell type', 'species', 'region', 'lab']
+    for category in ['cell type', 'species', 'region', 'lab']:
+        df[category] = df[category].apply(remove_sep)
+    df.drop_duplicates(inplace=True)
+    
+    for category in categories:
+        df2 = add_count_col(df, category)
+        df2 = pd.merge(df, df2)
+        max_vals = {}
+        for name, group in df2.groupby('neuron name'):
+            counts = list(group['count'])
+            vals = list(group[category])
+            count_vals = zip(counts, vals)
+            max_count, max_val = max(count_vals)
+            max_vals[name] = max_val
+        df['max ' + category] = df['neuron name'].map(max_vals)
+        df = df[df[category] == df['max ' + category]]
+        df.drop('max ' + category, axis=1, inplace=True)
+
+    df.to_csv(CATEGORIES_FILE_FILTERED, index=False)
+
 def main():
-    write_categories()
+    #write_categories()
+    filter_categories()
 
 if __name__ == '__main__':
     main()
